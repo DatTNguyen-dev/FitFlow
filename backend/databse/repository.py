bcc9542bbd2a268040766ba2949d5cf36cf455e1
BlueTAnh <tuananhm2007@gmail.com>
2026-01-30 22:42:09 +0700

    feat(db): add repository functions for training plans and tasks

--- backend/databse/repository.py
+++ backend/databse/repository.py
@@ -21,3 +21,43 @@ def create_user():
         "id": user_id,
         "created_at": created_at
     }
+
+def create_training_plans(user_id, start_date, end_date):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    plan_id = str(uuid.uuid4())
+    created_at = datetime.utcnow().isoformat()
+
+    cursor.execute(
+        """
+        INSERT INTO training_plans (id, user_id, start_date, end_date, created_at)
+        VALUES(?, ?, ?, ?, ?)
+        """,
+        (plan_id, user_id, start_date, end_date, created_at)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return plan_id
+
+def add_training_task(plan_id, name, duration_minutes, scheduled_date):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    task_id = str(uuid.uuid4())
+
+    cursor.execute(
+        """
+        INSERT INTO training_tasks (id, plan_id, name, duration_minutes, scheduled_date)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (task_id, plan_id, name, duration_minutes, scheduled_date)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return task_id
+
